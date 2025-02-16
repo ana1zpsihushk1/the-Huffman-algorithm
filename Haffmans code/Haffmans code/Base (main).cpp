b8ceeab2aeacba1de1f2ac8d39afef487984f121
Minasabu <nastabuinskaa@gmail.com>
2025-02-16 23:54:53 +0300

    remake of main function

--- Haffmans code/Haffmans code/Base (main).cpp
+++ Haffmans code/Haffmans code/Base (main).cpp
@@ -5,21 +5,50 @@
 
 int main()
 {
-	FILE* fin = fopen("input.txt", "r");
-	if (!fin)
-	{
-		fprintf(stderr, "Can`t open dile for reading\n");
-		return -1;
-	}
+    int freq[ALPHABET] = { 0 };
 
-	FILE* fout = fopen("output.txt", "w");
-	if (!fout)
-	{
-		fprintf(stderr, "Can`t open dile for reading\n");
-		return -2;
-	}
+    // Читаем частоты символов
+    FILE* fin = fopen("input.txt", "rb");
+    if (!fin) {
+        fprintf(stderr, "Can't open input file\n");
+        return -1;
+    }
 
-	fclose(fin);
-	fclose(fout);
-	return 0;
+    fseek(fin, 0L, SEEK_END);
+    long int length = ftell(fin);
+    fseek(fin, 0, SEEK_SET);
+
+    for (int i = 0; i < length; i++) {
+        freq[(unsigned char)fgetc(fin)]++;
+    }
+    fclose(fin);
+
+    // Создаем дерево
+    NODE* root = buildTree(freq);
+    if (!root) {
+        fprintf(stderr, "Error: failed to build Huffman tree\n");
+        return -1;
+    }
+
+    // Генерируем коды
+    char codes[ALPHABET][MAX_CODE_SIZE] = { 0 };
+    char tempCode[MAX_CODE_SIZE];
+    //generateHuffmanCodes(root, codes, tempCode, 0);
+
+    // Кодируем файл
+    fin = fopen("input.txt", "rb");
+    FILE* fout = fopen("encoded.txt", "wb"); //было bin
+    encodeFile(fin, fout, codes);
+    fclose(fin);
+    fclose(fout);
+
+    // Декодируем файл
+    fin = fopen("encoded.txt", "rb");
+    fout = fopen("decoded.txt", "wb");
+    //decodeFile(fin, fout, root);
+    fclose(fin);
+    fclose(fout);
+
+    freeTree(root);
+    return 0;
 }
