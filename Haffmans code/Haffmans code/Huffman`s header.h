061b097d8239c10e3271e9cca767ad83869026b9
Erena18 <alenat457@gmail.com>
2025-02-16 23:47:20 +0300

    Declarations of funcs

--- Haffmans code/Haffmans code/Huffman`s header.h
+++ Haffmans code/Haffmans code/Huffman`s header.h
@@ -44,9 +44,9 @@ void enQueue(QUEUE* queue, NODE* node);
 NODE* deQueue(QUEUE* queue);
 
 // Дерево Хаффмана
-NODE* buildTree(int* freq);
 NODE* createNode(unsigned char symb, unsigned int freq);
 NODE* MakeNodeFromNode(NODE* left, NODE* right);
 void Add2List(NODE** head, NODE* newNode);
+NODE* buildTree(int* freq);
 
 #endif
