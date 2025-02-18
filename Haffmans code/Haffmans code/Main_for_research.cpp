877226a757a04292e2df5acdd6ab88de032ec9f2
Minasabu <nastabuinskaa@gmail.com>
2025-02-18 15:43:12 +0300

    New Main Main file (final)

--- Haffmans code/Haffmans code/Main_for_research.cpp
+++ Haffmans code/Haffmans code/Main_for_research.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -70,3 +70,5 @@ int main()
     measureExecutionTime(decode, "Decoding", filename, freq);
     return 0;
 }
+
+*/
