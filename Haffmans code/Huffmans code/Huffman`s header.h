79aec53824ddb44d73e28a804469cc8445883f6c
Minasabu <nastabuinskaa@gmail.com>
2025-02-20 17:43:52 +0300

    add files

---
+++ Haffmans code/Huffmans code/Huffman`s header.h
@@ -0,0 +1,63 @@
+#ifndef HUFFMAN
+#define HUFFMAN
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ALPHABET 256
+#define MAX_CODE_SIZE 256
+
+typedef struct Node
+{
+    unsigned char symb;
+    unsigned int freq;
+    struct Node* left, * right, * next;
+}NODE;
+
+//Очередь(с приоритетами)
+typedef struct Queue
+{
+    NODE* nodes[ALPHABET];
+    int size;
+} QUEUE;
+
+// Битовый буфер
+typedef union bit2char
+{
+    char symb;
+    struct bit
+    {
+        unsigned b1 : 1;
+        unsigned b2 : 1;
+        unsigned b3 : 1;
+        unsigned b4 : 1;
+        unsigned b5 : 1;
+        unsigned b6 : 1;
+        unsigned b7 : 1;
+        unsigned b8 : 1;
+    } mbit;
+}BIT2CHAR;
+
+// Очередь (с приоритетами)
+QUEUE* createQueue();
+void enQueue(QUEUE* queue, NODE* node);
+NODE* deQueue(QUEUE* queue);
+
+// Дерево Хаффмана
+NODE* createNode(unsigned char symb, unsigned int freq);
+NODE* MakeNodeFromNode(NODE* left, NODE* right);
+void Add2List(NODE** head, NODE* newNode);
+NODE* buildTree(int* freq);
+
+// Кодирование и декодирование
+void generateHuffmanCodes(NODE* root, char codes[ALPHABET][MAX_CODE_SIZE], char* code, int level);
+void encodeFile(FILE* inputFile, FILE* outputFile, char codes[ALPHABET][MAX_CODE_SIZE]);
+void decodeFile(FILE* inputFile, FILE* outputFile, NODE* root);
+
+void loadCodesFromFile(char codes[ALPHABET][MAX_CODE_SIZE], const char* filename);
+void saveCodesToFile(const char codes[ALPHABET][MAX_CODE_SIZE], const char* filename);
+
+// Освобождение памяти
+void freeTree(NODE* root);
+
+#endif
---
+++ Haffmans code/Huffmans code/Main_for_research.cpp
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Huffman`s header.h"
+#include <time.h>
+
+void measureExecutionTime(void (*func)(const char*, int*), const char* description, const char* filename, int* freq)
+{
+    clock_t start_time = clock();
+    func(filename, freq);
+    clock_t end_time = clock();
+    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("%s time: %f seconds\n", description, time_taken);
+}
+
+void encode(const char* filename, int* freq)
+{
+    NODE* root = buildTree(freq);
+    char codes[ALPHABET][MAX_CODE_SIZE] = { 0 };
+    char tempCode[MAX_CODE_SIZE];
+    generateHuffmanCodes(root, codes, tempCode, 0);
+
+    FILE* fin = fopen(filename, "rb");
+    FILE* fout = fopen("encoded.bin", "wb");
+    encodeFile(fin, fout, codes);
+    fclose(fin);
+    fclose(fout);
+
+    saveCodesToFile(codes, "codes.txt");
+    freeTree(root);
+}
+
+void decode(const char* filename, int* freq)
+{
+    NODE* root = buildTree(freq);
+    char codes[ALPHABET][MAX_CODE_SIZE] = { 0 };
+    char tempCode[MAX_CODE_SIZE];
+    generateHuffmanCodes(root, codes, tempCode, 0);
+
+    FILE* f = fopen("encoded.bin", "rb");
+    FILE* fout = fopen("decoded.mov", "wb"); ///
+    decodeFile(f, fout, root);
+    fclose(f);
+    fclose(fout);
+
+    freeTree(root);
+}
+
+int main()
+{
+    int freq[ALPHABET] = { 0 };
+    const char* filename = "42 MB.mov";
+    FILE* fin = fopen(filename, "rb");
+    if (!fin) 
+    {
+        fprintf(stderr, "Error: cannot open codes file\n");
+        exit(1);
+    }
+    fseek(fin, 0L, SEEK_END);
+    long int length = ftell(fin);
+    fseek(fin, 0, SEEK_SET);
+    for (int i = 0; i < length; i++)
+    {
+        freq[(unsigned char)fgetc(fin)]++;
+    }
+    fclose(fin);
+
+    measureExecutionTime(encode, "Encoding", filename, freq);
+    measureExecutionTime(decode, "Decoding", filename, freq);
+    return 0;
+}
+
