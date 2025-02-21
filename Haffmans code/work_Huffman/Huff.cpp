a2d1421df586be3faadf97bad9e82c79fbb5edff
Minasabu <nastabuinskaa@gmail.com>
2025-02-21 06:07:37 +0300

    fixing bags

---
+++ Haffmans code/work_Huffman/Huff.cpp
@@ -0,0 +1,236 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Huff.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Очередь (с приоритетами)
+QUEUE* createQueue()
+{
+    QUEUE* queue = (QUEUE*)malloc(sizeof(QUEUE));
+    queue->size = 0; // Инициализация размера очереди
+    return queue;
+}
+
+void enQueue(QUEUE* queue, NODE* node) // Добавление узла в очередь
+{
+    queue->nodes[queue->size++] = node;
+}
+
+NODE* deQueue(QUEUE* queue) // Извлечение узла с наименьшей частотой из очереди
+{
+    if (queue->size == 0)
+    {
+        return NULL;
+    }
+    NODE* min_node = queue->nodes[0];
+    for (int i = 1; i < queue->size; i++)
+    {
+        queue->nodes[i - 1] = queue->nodes[i];
+    }
+    queue->size--;
+    return min_node;
+}
+
+NODE* createNode(unsigned char symb, unsigned int freq) // Создание узла
+{
+    NODE* node = (NODE*)malloc(sizeof(NODE));
+    node->symb = symb;
+    node->freq = freq;
+    node->left = node->right = NULL;
+    node->next = NULL;
+    return node;
+}
+
+NODE* MakeNodeFromNode(NODE* left, NODE* right) // Объединение узлов
+{
+    NODE* parent = createNode(0, left->freq + right->freq);
+    parent->left = left;
+    parent->right = right;
+    return parent;
+}
+
+// Добавление в отсортированный список
+void Add2List(NODE** head, NODE* newNode)
+{
+    if (!newNode)
+    {
+        return;
+    }
+    if (*head == NULL || newNode->freq < (*head)->freq)
+    {
+        newNode->next = *head;
+        *head = newNode;
+        return;
+    }
+    NODE* current = *head;
+    while (current->next && newNode->freq > current->next->freq)
+    {
+        current = current->next;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+}
+
+// Построение дерева
+NODE* buildTree(int* freq)
+{
+    NODE* head = NULL;
+    for (int i = 0; i < ALPHABET; i++)
+    {
+        if (freq[i] > 0)
+        {
+            NODE* node = createNode((unsigned char)i, freq[i]);
+            Add2List(&head, node);
+        }
+    }
+    while (head && head->next)
+    {
+        NODE* left = head;
+        NODE* right = head->next;
+        head = right->next;
+        NODE* parent = MakeNodeFromNode(left, right);
+        Add2List(&head, parent);
+    }
+    return head;
+}
+
+// Генерация кодов Хаффмана
+void generateHuffmanCodes(NODE* root, char codes[ALPHABET][MAX_CODE_SIZE], char* code, int level)
+{
+    if (!root)
+    {
+        return;
+    }
+    if (!root->left && !root->right)
+    {
+        code[level] = '\0';
+        strcpy(codes[root->symb], code);
+    }
+    else
+    {
+        code[level] = '0';
+        generateHuffmanCodes(root->left, codes, code, level + 1);
+        code[level] = '1';
+        generateHuffmanCodes(root->right, codes, code, level + 1);
+    }
+}
+
+// Кодирование файла побитово
+void encodeFile(FILE* inputFile, FILE* outputFile, char codes[ALPHABET][MAX_CODE_SIZE])
+{
+    /*int ch;
+    while ((ch = fgetc(inputFile)) != EOF)
+    {
+        fputs(codes[ch], outputFile);
+    }*/
+    BIT2CHAR bitBuff = { 0 };
+    int bitCnt = 0, ch;
+    while ((ch = fgetc(inputFile)) != EOF)
+    {
+        for (char* p = codes[ch]; *p; p++)
+        {
+            if (*p == '1')
+            {
+                bitBuff.symb |= (1 << (7 - bitCnt));
+            }
+            bitCnt++;
+            if (bitCnt == 8)
+            {
+                fputc(bitBuff.symb, outputFile);
+                bitBuff.symb = 0;
+                bitCnt = 0;
+            }
+        }
+    }
+    if (bitCnt > 0)
+    {
+        fputc(bitBuff.symb, outputFile);
+    }
+}
+
+// Декодирование файла
+void decodeFile(FILE* inputFile, FILE* outputFile, NODE* root)
+{
+    /*NODE* current = root;
+    int ch;
+    while ((ch = fgetc(inputFile)) != EOF) {
+        if (ch == '0') {
+            current = current->left;
+        }
+        else if (ch == '1') {
+            current = current->right;
+        }
+
+        if (!current->left && !current->right) {
+            fputc(current->symb, outputFile);
+            current = root;
+        }
+    }*/
+    NODE* current = root;
+    int ch;
+    while ((ch = fgetc(inputFile)) != EOF)
+    {
+        BIT2CHAR bitBuffer;
+        bitBuffer.symb = ch;
+
+        for (int i = 7; i >= 0; i--)
+        {
+            current = (bitBuffer.symb & (1 << i)) ? current->right : current->left;
+            if (!current->left && !current->right)
+            {
+                fputc(current->symb, outputFile);
+                current = root;
+            }
+        }
+    }
+}
+
+
+
+void loadCodesFromFile(char codes[ALPHABET][MAX_CODE_SIZE], const char* filename)
+{
+    FILE* file = fopen(filename, "r");
+    if (!file)
+    {
+        fprintf(stderr, "Error: cannot open codes file\n");
+        exit(1);
+    }
+    unsigned char symbol;
+    char code[MAX_CODE_SIZE];
+    while (fscanf(file, "%c %s\n", &symbol, code) == 2)
+    {
+        strcpy(codes[symbol], code);
+    }
+    fclose(file);
+}
+
+void saveCodesToFile(const char codes[ALPHABET][MAX_CODE_SIZE], const char* filename)
+{
+    FILE* file = fopen(filename, "w");
+    if (!file)
+    {
+        fprintf(stderr, "Error: Could not open %s\n", filename);
+        return;
+    }
+    for (int i = 0; i < ALPHABET; i++)
+    {
+        if (codes[i][0] != '\0')
+        {
+            fprintf(file, "%c: %s\n", (char)i, codes[i]);
+        }
+    }
+    fclose(file);
+}
+
+// Освобождение памяти дерева
+void freeTree(NODE* root)
+{
+    if (root)
+    {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
+}
---
+++ Haffmans code/work_Huffman/Main (base).cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Huff.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int freq[ALPHABET] = { 0 };
+
+    FILE* fin = fopen("TestMelody1.wav", "rb");
+    if (!fin) {
+        fprintf(stderr, "Can't open input file\n");
+        return -1;
+    }
+
+    fseek(fin, 0L, SEEK_END);
+    long int length = ftell(fin);
+    fseek(fin, 0, SEEK_SET);
+
+    for (int i = 0; i < length; i++) {
+        freq[(unsigned char)fgetc(fin)]++;
+    }
+    fclose(fin);
+
+    NODE* root = buildTree(freq);
+    if (!root) {
+        fprintf(stderr, "Error: failed to build Huffman tree\n");
+        return -1;
+    }
+
+    char codes[ALPHABET][MAX_CODE_SIZE] = { 0 };
+    char tempCode[MAX_CODE_SIZE];
+    generateHuffmanCodes(root, codes, tempCode, 0);
+
+    saveCodesToFile(codes, "codes.txt");
+
+    fin = fopen("TestMelody1.wav", "rb");
+    FILE* fout = fopen("encoded.bin", "wb");
+    encodeFile(fin, fout, codes);
+    fclose(fin);
+    fclose(fout);
+
+    char loadedCodes[ALPHABET][MAX_CODE_SIZE] = { 0 };
+    loadCodesFromFile(loadedCodes, "codes.txt");
+
+    fin = fopen("encoded.bin", "rb");
+    fout = fopen("decoded.wav", "wb");
+    decodeFile(fin, fout, root);
+    fclose(fin);
+    fclose(fout);
+
+    freeTree(root);
+    return 0;
+}
